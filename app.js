773129eb35952fa9bf5697657d45c5b433e0fd5c
Durmuş <demirtasdurmus@gmail.com>
2022-09-10 19:10:58 +0300

    ADD morgan for logging

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const path = require("path")
 const express = require("express")
 const cors = require('cors')
+const morgan = require("morgan")
 
 const app = express()
 
 app.use(cors())
+app.use(morgan("dev"))
 
 module.exports = app
